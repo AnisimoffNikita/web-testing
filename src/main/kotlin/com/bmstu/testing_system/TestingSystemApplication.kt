c58e1c93b781cd4a76fa4974d1560aeb98530072
Nikita <anisimoff.nikita@gmail.com>
2018-11-03 17:42:06 +0300

    init

---
+++ src/main/kotlin/com/bmstu/testing_system/TestingSystemApplication.kt
@@ -0,0 +1,13 @@
+package com.bmstu.testing_system
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+
+
+@SpringBootApplication
+class TestingSystemApplication
+
+fun main(args: Array<String>) {
+    runApplication<TestingSystemApplication>(*args)
+}
