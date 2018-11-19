f35be514d5de3a6ad7fb983b47cdfbb0ec559202
lel9 <olg-199774@mail.ru>
2018-11-19 19:54:33 +0300

    убрала кнопку "отправить аватар"

--- src/main/kotlin/com/bmstu/testingsystem/services/UserService.kt
+++ src/main/kotlin/com/bmstu/testingsystem/services/UserService.kt
@@ -3,6 +3,7 @@ package com.bmstu.testingsystem.services
 import com.bmstu.testingsystem.controller.SignUp
 import com.bmstu.testingsystem.domain.User
 import com.bmstu.testingsystem.form_data.UserData
+import org.springframework.web.multipart.MultipartFile
 
 
 interface UserService {
@@ -13,7 +14,7 @@ interface UserService {
 
     fun updateUser(user: User, newUserData: UserData): Boolean
 
-    fun updateAvatar(user: User, path: String)
+    fun updateAvatar(user: User, newAvatar: String, file: MultipartFile)
 
     fun getAvatar(user: User): String
 }
