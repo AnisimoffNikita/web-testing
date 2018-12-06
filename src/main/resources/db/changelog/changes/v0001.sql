8366731adbb718a33b3f871da51f2e9db1bb3834
Nikita <anisimoff.nikita@gmail.com>
2018-12-07 00:40:28 +0300

    fix travis

--- src/main/resources/db/changelog/changes/v0001.sql
+++ src/main/resources/db/changelog/changes/v0001.sql
@@ -50,24 +50,24 @@ CREATE TABLE exams (
 );
 
 INSERT INTO exams (id, user_id, name, description, pass_count, status, created_at, questions)
-    VALUES ('0596c2c0-a70a-47dd-81c8-31411a5b132a', '12412cdb-398f-4def-9cec-325b11968b56', 'тест главный', 'большое описание со словом математика', 8, 'APPROVED', to_timestamp(1195333200)::timestamp without time zone, '[]'::json);
+    VALUES ('0596c2c0-a70a-47dd-81c8-31411a5b132a', '12412cdb-398f-4def-9cec-325b11968b56', 'тест главный', 'большое описание со словом математика', 8, 'APPROVED', to_timestamp(1195333200)::timestamp with time zone, '[]'::json);
 INSERT INTO exams (id, user_id, name, description, pass_count, status, created_at, questions)
-    VALUES ('66bcd4a3-a3d5-409e-9a38-e0d7b029a020', '7c803c41-ca5f-4e66-9483-7e361db72917', 'тест номер два', 'описание теста номер два', 10, 'APPROVED', to_timestamp(1195333200)::timestamp without time zone, '[]'::json);
+    VALUES ('66bcd4a3-a3d5-409e-9a38-e0d7b029a020', '7c803c41-ca5f-4e66-9483-7e361db72917', 'тест номер два', 'описание теста номер два', 10, 'APPROVED', to_timestamp(1195333200)::timestamp with time zone, '[]'::json);
 
 INSERT INTO exams (id, user_id, name, description, pass_count, status, created_at, questions)
     VALUES ('446ae2f3-eb60-44cb-b889-22f14ef06d82', 'be7b8ae6-5368-40f1-9b07-b33027acf43f', 'тестик', 'много слов', 3, 'APPROVED', to_timestamp(1195333200)::timestamp without time zone, '[{"id":0,"questionText":"1+1","type":"SINGLE_ANSWER","variants":["1","2"],"correctVariantsId":[1],"correctInputAnswer":null}]'::json);
 
 INSERT INTO exams (id, user_id, name, description, pass_count, status, created_at, questions)
-    VALUES ('6c8d5358-4111-4d28-a12e-fecfa3c1ce78', 'ecc4f8af-c758-434a-84c1-6df04cbb0936', 'тест второго юзера', 'длинное описание', 10, 'APPROVED', to_timestamp(1195333200)::timestamp without time zone, '[]'::json);
+    VALUES ('6c8d5358-4111-4d28-a12e-fecfa3c1ce78', 'ecc4f8af-c758-434a-84c1-6df04cbb0936', 'тест второго юзера', 'длинное описание', 10, 'APPROVED', to_timestamp(1195333200)::timestamp with time zone, '[]'::json);
 
 INSERT INTO exams (id, user_id, name, description, pass_count, status, created_at, questions)
-    VALUES ('e17b57b3-232f-4405-82e7-f21f9ef93c1a', '0d984722-ba7f-4b57-9afb-3078191f2a01', 'ненужный тест', 'тест, который будет удален юзером 3', 0, 'APPROVED', to_timestamp(1195333200)::timestamp without time zone, '[]'::json);
+    VALUES ('e17b57b3-232f-4405-82e7-f21f9ef93c1a', '0d984722-ba7f-4b57-9afb-3078191f2a01', 'ненужный тест', 'тест, который будет удален юзером 3', 0, 'APPROVED', to_timestamp(1195333200)::timestamp with time zone, '[]'::json);
 INSERT INTO exams (id, user_id, name, description, pass_count, status, created_at, questions)
-    VALUES ('6d94c110-43f1-433f-87fa-9ce0fb81edcc', '0d984722-ba7f-4b57-9afb-3078191f2a01', 'еще один ненужный тест', 'тест, который будет удален админом', 0, 'APPROVED', to_timestamp(1195333200)::timestamp without time zone, '[]'::json);
+    VALUES ('6d94c110-43f1-433f-87fa-9ce0fb81edcc', '0d984722-ba7f-4b57-9afb-3078191f2a01', 'еще один ненужный тест', 'тест, который будет удален админом', 0, 'APPROVED', to_timestamp(1195333200)::timestamp with time zone, '[]'::json);
 INSERT INTO exams (id, user_id, name, description, pass_count, status, created_at, questions)
-    VALUES ('a7f330fb-9a1a-492f-a52c-3a5be37ccff4', '0d984722-ba7f-4b57-9afb-3078191f2a01', 'ожидающий тест', 'тест, который будет добавлен админом', 0, 'PENDING', to_timestamp(1195333200)::timestamp without time zone, '[]'::json);
+    VALUES ('a7f330fb-9a1a-492f-a52c-3a5be37ccff4', '0d984722-ba7f-4b57-9afb-3078191f2a01', 'ожидающий тест', 'тест, который будет добавлен админом', 0, 'PENDING', to_timestamp(1195333200)::timestamp with time zone, '[]'::json);
 INSERT INTO exams (id, user_id, name, description, pass_count, status, created_at, questions)
-    VALUES ('4dfe3450-b8eb-4d50-9f0b-01b324a553b1', '0d984722-ba7f-4b57-9afb-3078191f2a01', 'еще один ожидающий тест', 'тест, который будет отклонен админом', 0, 'PENDING', to_timestamp(1195333200)::timestamp without time zone, '[]'::json);
+    VALUES ('4dfe3450-b8eb-4d50-9f0b-01b324a553b1', '0d984722-ba7f-4b57-9afb-3078191f2a01', 'еще один ожидающий тест', 'тест, который будет отклонен админом', 0, 'PENDING', to_timestamp(1195333200)::timestamp with time zone, '[]'::json);
 
 CREATE TABLE exam_results (
   id UUID PRIMARY KEY NOT NULL,
@@ -78,15 +78,15 @@ CREATE TABLE exam_results (
 );
 
 INSERT INTO exam_results  (id, user_id, test_id, result, passed_at)
-    VALUES ('18c4f984-22bd-4edd-ae66-6fb157328337', '12412cdb-398f-4def-9cec-325b11968b56', '0596c2c0-a70a-47dd-81c8-31411a5b132a', '11', to_timestamp(1195333200)::timestamp without time zone);
+    VALUES ('18c4f984-22bd-4edd-ae66-6fb157328337', '12412cdb-398f-4def-9cec-325b11968b56', '0596c2c0-a70a-47dd-81c8-31411a5b132a', '11', to_timestamp(1195333200)::timestamp with time zone);
 
 INSERT INTO exam_results  (id, user_id, test_id, result, passed_at)
-    VALUES ('9d7c4fc9-56bb-4e39-b359-f23d56c3bfe2', '7c803c41-ca5f-4e66-9483-7e361db72917', '66bcd4a3-a3d5-409e-9a38-e0d7b029a020', '12', to_timestamp(1195333200)::timestamp without time zone);
+    VALUES ('9d7c4fc9-56bb-4e39-b359-f23d56c3bfe2', '7c803c41-ca5f-4e66-9483-7e361db72917', '66bcd4a3-a3d5-409e-9a38-e0d7b029a020', '12', to_timestamp(1195333200)::timestamp with time zone);
 INSERT INTO exam_results  (id, user_id, test_id, result, passed_at)
-    VALUES ('5f4f4ac2-bd8f-4193-847c-7fef7bb4c7ab', 'be7b8ae6-5368-40f1-9b07-b33027acf43f', '66bcd4a3-a3d5-409e-9a38-e0d7b029a020', '1/2', to_timestamp(1195333200)::timestamp without time zone);
+    VALUES ('5f4f4ac2-bd8f-4193-847c-7fef7bb4c7ab', 'be7b8ae6-5368-40f1-9b07-b33027acf43f', '66bcd4a3-a3d5-409e-9a38-e0d7b029a020', '1/2', to_timestamp(1195333200)::timestamp with time zone);
 
 INSERT INTO exam_results  (id, user_id, test_id, result, passed_at)
-    VALUES ('74f7a1b8-5bbd-411f-82a3-9c2aa1e9ab3e', 'ecc4f8af-c758-434a-84c1-6df04cbb0936', '446ae2f3-eb60-44cb-b889-22f14ef06d82', '1/3', to_timestamp(1195333200)::timestamp without time zone);
+    VALUES ('74f7a1b8-5bbd-411f-82a3-9c2aa1e9ab3e', 'ecc4f8af-c758-434a-84c1-6df04cbb0936', '446ae2f3-eb60-44cb-b889-22f14ef06d82', '1/3', to_timestamp(1195333200)::timestamp with time zone);
 
 INSERT INTO exam_results  (id, user_id, test_id, result, passed_at)
-    VALUES ('073b22c6-4deb-40cf-99d5-4b36c1e41478', '0d984722-ba7f-4b57-9afb-3078191f2a01', '6c8d5358-4111-4d28-a12e-fecfa3c1ce78', '3/3', to_timestamp(1195333200)::timestamp without time zone);
+    VALUES ('073b22c6-4deb-40cf-99d5-4b36c1e41478', '0d984722-ba7f-4b57-9afb-3078191f2a01', '6c8d5358-4111-4d28-a12e-fecfa3c1ce78', '3/3', to_timestamp(1195333200)::timestamp with time zone);
